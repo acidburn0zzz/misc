d3b9a571e3637758cada8bc41d91945d876fd5b3
acidrain42 <acidrain1@gmail.com>
2009-05-04 11:25:04 +0000

    Optimisation de crc32_hash Correction d'un epic fail sur crc32_hash_file
    git-svn-id: http://svn2.xp-dev.com/svn/acidrain@34 6e852721-9c5f-4ab4-8f73-299ae3065a95

--- crc32/main.c
+++ crc32/main.c
@@ -7,15 +7,15 @@
 int main(int argc, char **argv) {
     uint32_t crc32;
     
-	if (argc != 2) {
-		fprintf(stderr, "%s: No input file\n", argv[0]);
-		exit (EXIT_FAILURE);
-	}
+    if (argc != 2) {
+        fprintf(stderr, "%s: No input file\n", argv[0]);
+        exit (EXIT_FAILURE);
+    }
     
     if (crc32_hash_file(argv[1], &crc32) != 0)
-		exit (EXIT_FAILURE);
+        exit (EXIT_FAILURE);
     
-	printf("%s: %.8x\n", argv[1], crc32);
-	
-	return EXIT_SUCCESS;
+    printf("%s: %.8x\n", argv[1], crc32);
+    
+    return EXIT_SUCCESS;
 }
--- libcrc32/crc32.c
+++ libcrc32/crc32.c
@@ -27,11 +27,8 @@ void crc32_begin(uint32_t *sum) {
 }
 
 void crc32_hash(const unsigned char *data, unsigned int len, uint32_t *sum) {
-    unsigned int i;
-    
-    for (i=0; i<len; i++) {
-        *sum = (*sum >> 8) ^ crc_table[(*sum ^ data[i]) & 0xff];
-    }
+    while (len--)
+        *sum = (*sum >> 8) ^ crc_table[(*sum ^ *data++) & 0xff];
 }
 
 void crc32_end(uint32_t *sum) {
@@ -50,51 +47,52 @@ unsigned int crc32_hash_string(unsigned char *str) {
 
 int crc32_hash_file(char *fn, uint32_t *sum) {
     FILE *f;
-	const int BUFFER_SIZE=16384;
-	unsigned int size;
-	unsigned char buffer[16384];
-	
-	if (!(f = fopen(fn, "r"))) {
-		fprintf(stderr, "Error opening file %s\n", fn);
-		return -1;
-	}
-	
-	fseek(f, 0, SEEK_END);
-	size = ftell(f);
-	fseek(f, 0, SEEK_SET);
-	
-	crc32_begin(sum);
-	
-	while (size > 0) {
-		if (size < BUFFER_SIZE) {
-			if (fread(buffer, size, 1, f) != 1) {
+    const int BUFFER_SIZE=16384;
+    unsigned int size;
+    unsigned char buffer[16384];
+    
+    if (!(f = fopen(fn, "r"))) {
+        fprintf(stderr, "Error opening file %s\n", fn);
+        return -1;
+    }
+    
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    
+    crc32_begin(sum);
+    
+    while (size > 0) {
+        if (size < BUFFER_SIZE) {
+            if (fread(buffer, size, 1, f) != 1) {
                 fprintf(stderr, "Error reading file %s\n", fn);
                 return -1;
             }
-			size = 0;
-		} else {
-			if (fread(buffer, size, 1, f) != 1) {
+            crc32_hash(buffer, size, sum);
+            size = 0;
+        } else {
+            if (fread(buffer, BUFFER_SIZE, 1, f) != 1) {
                 fprintf(stderr, "Error reading file %s\n", fn);
                 return -1;
             }
-			size-=BUFFER_SIZE;
-		}
-		crc32_hash(buffer, strlen((char*)buffer), sum);
-	}
-	
-	crc32_end(sum);
+            crc32_hash(buffer, BUFFER_SIZE, sum);
+            size-=BUFFER_SIZE;
+        }
+    }
+    
+    crc32_end(sum);
     
     return 0;
 }
 
 int crc32_test() {
-    unsigned int foo;
+    uint32_t foo;
     crc32_begin(&foo);
     crc32_hash((unsigned char*)"Sample string", 13, &foo);
     crc32_end(&foo);
     
     if (foo == 0xa7579504)
-        return 1;
-    else
         return 0;
+    else
+        return -1;
 }
