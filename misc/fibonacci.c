ff18b4c1238ff366d05f3e37026d655b31c06fe2
acidrain42 <acidrain1@gmail.com>
2009-07-24 11:20:46 +0000

    fibonacci: f(0) = 0, not 1
    git-svn-id: http://svn2.xp-dev.com/svn/acidrain@70 6e852721-9c5f-4ab4-8f73-299ae3065a95

--- misc/fibonacci.c
+++ misc/fibonacci.c
@@ -6,24 +6,24 @@ double count = 0;
 
 long int fibonacci(int n) {
     count++;
-    return (n <= 1)? 1 : fibonacci(n-1) + fibonacci(n-2);
+    return (n <= 1)? n : fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main(int argc, char *argv[]) {
     long int n;
     int r, t;
-    
+
     if (argc == 2) {
         sscanf(argv[1], "%li", &n);
     } else {
         printf("x = ");
         scanf("%li", &n);
     }
-    
+
     t = time(NULL);
     r = fibonacci(n);
     t = time(NULL) - t;
-    
+
     printf("fibonacci(%li) = %i\n%.0f calculs\nTook %i seconds\n", n, r, count, t);
     return EXIT_SUCCESS;
 }
