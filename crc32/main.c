923bd1ec6fd969a773c676b09b7a6aed8ef7666b
acidrain42 <acidrain1@gmail.com>
2009-08-04 15:53:25 +0000

    Correction de usage, ajout d'un param pour upper/lower case (lower par defaut)
    git-svn-id: http://svn2.xp-dev.com/svn/acidrain@72 6e852721-9c5f-4ab4-8f73-299ae3065a95

--- crc32/main.c
+++ crc32/main.c
@@ -14,9 +14,9 @@
 void usage(char *prog) {
     fprintf(stderr, "AcidCRC32 %s\n", __PROG_VERSION__);
     fprintf(stderr, "Usage :\n");
-    fprintf(stderr, "\t%s [-pv] -c sfv_file\n", prog);
+    fprintf(stderr, "\t%s [-u] file(s)\n", prog);
     fprintf(stderr, "OR\n");
-    fprintf(stderr, "\t%s [-v] file(s)\n", prog);
+    fprintf(stderr, "\t%s [-u] -c sfv_file\n", prog);
     exit(EXIT_FAILURE);
 }
 
@@ -24,27 +24,28 @@ void print_header() {
     time_t rawtime;
     struct tm *timeinfo;
     char buffer[128];
-    
+
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     if (strftime(buffer, 128, "le %d/%m/%Y a %X", timeinfo) == 0)
         buffer[0] = 0;
-    
+
     printf("; Genere par AcidCRC32 %s %s\n;\n", __PROG_VERSION__, buffer);
-}  
+}
 
 int main(int argc, char **argv) {
     uint32_t crc32;
     char opt, *sfv_file = NULL;
     int sfv = 0;
-    
+    int upper_case = 0;
+
     if (argc < 2) {
         usage(argv[0]);
         exit (EXIT_FAILURE);
     }
-    
+
     /* Traitement des arguments */
-    while ((opt = getopt(argc, argv, "c:h")) != -1) {
+    while ((opt = getopt(argc, argv, "c:hu")) != -1) {
         switch (opt) {
         case 'c':
             sfv_file = malloc(strlen(optarg) + 1);
@@ -53,30 +54,33 @@ int main(int argc, char **argv) {
             break;
         case 'h':
             usage(argv[0]);
-            break;
-        default:
-            usage(argv[0]);
+            exit(EXIT_SUCCESS);
+        case 'u':
+            upper_case = 1;
         }
     }
-            
+
     argc -= optind;
     argv += optind;
-    
+
     if (sfv) {
         check_sfv_file(sfv_file);
         free(sfv_file);
         exit(EXIT_SUCCESS);
     }
-    
+
     print_header();
-    
+
     while(argc--) {
         if (crc32_hash_file(*argv, &crc32) != 0)
             exit (EXIT_FAILURE);
-        printf("%s %.8x\n", *argv, crc32);
-        
+        if (upper_case)
+            printf("%s %.8X\n", *argv, crc32);
+        else
+            printf("%s %.8x\n", *argv, crc32);
+
         argv++;
     }
-    
+
     return EXIT_SUCCESS;
 }
