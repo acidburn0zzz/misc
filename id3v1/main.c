d665a9ab493371bba49eb76bd53d3ebd53a3f568
acidrain <acidrain1@gmail.com>
2010-06-09 14:26:49 -0400

    usage of unsigned char, usage of LF instead of CRLF

--- id3v1/main.c
+++ id3v1/main.c
@@ -13,16 +13,16 @@ void showHelp() {
 int main(int argc, char *argv[]) {
     FILE *mp3;
     struct ID3 id3;
-    
+
     int b_id3=0, opt, f_show=0, f_verbose=0, index;
-    char *title=NULL, *artist=NULL, *album=NULL, *year=NULL, *comment=NULL, track='\0', *genre=NULL, *filename=NULL;
+    unsigned char *title=NULL, *artist=NULL, *album=NULL, *year=NULL, *comment=NULL, track='\0', *genre=NULL, *filename=NULL;
 
     if (argc < 3) {
         fprintf(stderr, "Usage: %s options mp3_file\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    //Options
+
+    /*Options*/
     while ((opt = getopt(argc, argv, "a:c:g:hl:r:st:vy:")) != -1) {
         switch (opt) {
             case 'a':
@@ -57,36 +57,36 @@ int main(int argc, char *argv[]) {
                 break;
         }
     }
-    
+
     if (optind != argc-1) {
         fprintf(stderr, "No file specified.\n");
         exit(EXIT_FAILURE);
     }
-    
+
     filename = argv[argc-1];
-    
-    
-    //Lire les 128 derniers bits du fichier
+
+
+    /*Lire les 128 derniers bits du fichier*/
     if ((mp3 = fopen(filename, "rb")) == NULL) {
         fprintf(stderr, "File %s not found.\n", filename);
         exit(EXIT_FAILURE);
     }
-    
+
     fseek(mp3, -128, SEEK_END);
-    
+
     if (fread(&id3, sizeof(id3), 1, mp3) == 0) {
         fprintf(stderr, "Impossible to read from %s.\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    
+
     fclose(mp3);
-    
-    //Verification si le tag existe
+
+    /*Verification si le tag existe*/
     if (id3.tag[0] == 'T' && id3.tag[1] == 'A' && id3.tag[2] == 'G') {
         b_id3 = 1;
     }
-    
-    //Afficher les infos si "-s" et quitter
+
+    /*Afficher les infos si "-s" et quitter*/
     if (f_show) {
         if (b_id3) {
             fprintf(stdout, "File : %s\n", filename);
--- id3v1/main.h
+++ id3v1/main.h
@@ -2,14 +2,14 @@
 #define _MAIN_H_
 
 struct ID3 {
-    char tag[3];
-    char title[30];
-    char artist[30];
-    char album[30];
-    char year[4];
-    char comment[29];
-    char track;
-    char genre;
+    unsigned char tag[3];
+    unsigned char title[30];
+    unsigned char artist[30];
+    unsigned char album[30];
+    unsigned char year[4];
+    unsigned char comment[29];
+    unsigned char track;
+    unsigned char genre;
 };
 
-#endif //_MAIN_H_
+#endif /*_MAIN_H_*/
