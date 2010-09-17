49510cb7b3f97a3e47bec656f0a7a773633c5b18
acidrain42 <acidrain1@gmail.com>
2010-09-17 14:37:59 -0400

    Transformed pid to a library

--- misc/pid.c
+++ misc/pid.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
+#include <pcre.h>
 
-void write_pid() {
+void write_pid(const char *pid_file) {
     FILE *f_pid;
 
-    f_pid = fopen("app.pid", "w");
+    f_pid = fopen(pid_file, "w");
     if (f_pid == NULL) {
-        perror("app.pid");
+        perror(pid_file);
         exit(EXIT_FAILURE);
     }
 
@@ -15,18 +17,24 @@ void write_pid() {
     fclose(f_pid);
 }
 
-int already_running() {
+/*
+ * Check if application is running at pid specified in file
+ * Returns : 0 if application not running, -1 if pid file exists but can't be read
+ *           pid of running app in other cases.
+ */
+int already_running(const char *pid_file, const char *app_name) {
     FILE *f;
+    pcre *regex_app;
+    const char *error;
+    int erroffset;
     char s_pid[9], cmd_file[1024], cmd_line[1024];
     int pid;
 
-    f = fopen("app.pid", "r");
+    f = fopen(pid_file, "r");
     if (f == NULL) {
-        perror("app.pid");
-
         if (errno == EACCES) {
             /* Permission denied, assume running */
-            return 1;
+            return -1;
         } else if (errno == ENOENT) {
             /* File not found, assume not running */
             return 0;
@@ -46,10 +54,20 @@ int already_running() {
         return 0;
     }
 
+    regex_app = pcre_compile(app_name, 0, &error, &erroffset, NULL);
+    if (regex_app == NULL) {
+        fprintf(stderr, "%s at %d\n", error, erroffset);
+        exit(EXIT_FAILURE);
+    }
+
     sprintf(cmd_file, "/proc/%d/cmdline", pid);
     f = fopen(cmd_file, "r");
     if (f == NULL) {
-        /* Check error for FileNotFound */
+        if (errno == ENOENT) {
+            /* file / pid does not exist */
+            return 0;
+        }
+        /* TODO: Remove or better check */
         /* Nothing found at pid */
         return 0;
     }
@@ -57,17 +75,9 @@ int already_running() {
     fgets(cmd_line, 1023, f);
     fclose(f);
 
-    printf("%s: %s\n", cmd_file, cmd_line);
-
-    return 1;
-}
-
-int main(int argc, char **argv) {
-    if (already_running()) {
-        fprintf(stderr, "Application is already running\n");
-        return EXIT_FAILURE;
+    if (pcre_exec(regex_app, NULL, cmd_line, strlen(cmd_line), 0, 0, NULL, 0) == 0) {
+        return pid;
     }
-    write_pid();
-    getchar();
+
     return 0;
 }
