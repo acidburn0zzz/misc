9df6c6ccb61a2e2b76727ca3bcd8658f934ddf1b
acidrain42 <acidrain1@gmail.com>
2010-09-27 12:11:14 -0400

    Added script to create thumbnails page from a directory

--- scripts/ism.sh
+++ scripts/ism.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-rename -v 's/_s\.zip/\.zip/' *.zip
+rename 's/_s\.zip/\.zip/' *.zip
 
 for i in $(ls *.zip); do
     name=`echo $i | awk -F . '{print $1}'`;
     mkdir $name;
-    unzip $i -d $name;
-    mv $name/*.mpg .;
+    unzip -q $i -d $name;
+    if [ -e $name/$name.mpg ];
+    then
+        mv $name/$name.mpg .;
+        rm -rf $name;
+    fi
     mv $name/*.html .;
     rm $i;
 done
---
+++ scripts/thumb.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+COUNT=0
+SIZE=150
+NBPL=7
+let LAST=NBPL-1
+
+type -P convert &>/dev/null || { echo "I require convert but it's not installed.  Aborting." >&2; exit 1; }
+type -P mogrify &>/dev/null || { echo "I require mogrify but it's not installed.  Aborting." >&2; exit 1; }
+
+if [ $# -gt 0 ];
+then
+    SIZE=$1
+fi
+
+if [ ! -e thumbs ];
+then
+    mkdir thumbs;
+fi
+
+cat << EOF > index.html
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Gallery</title>
+        <style type="text/css">
+            img {
+                border: none;
+            }
+            table {
+                margin: auto;
+            }
+            td {
+                padding: 10px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <table>
+EOF
+
+for i in $(ls *.jpg *.png 2>/dev/null); do
+    convert -thumbnail $SIZE $i thumbs/$i;
+    mogrify -quality '75%' thumbs/$i;
+
+    let MOD=COUNT%NBPL
+    if [ $MOD = 0 ];
+    then
+        echo "            <tr>" >> index.html
+    fi
+
+    echo "                <td><a href=\"$i\"><img src=\"thumbs/$i\" alt=\"$i\" /></a></td>" >> index.html
+
+    if [ $MOD = $LAST ];
+    then
+        echo "            </tr>" >> index.html
+    fi
+
+    let COUNT=COUNT+1
+done
+
+let MOD=COUNT%NBPL
+
+if [ $MOD != 0 ];
+then
+    while [ $MOD -lt $NBPL ]; do
+        echo "                <td>&nbsp;</td>" >> index.html;
+        let MOD=MOD+1;
+    done
+
+    echo "            </tr>" >> index.html
+fi
+
+cat << EOF >> index.html
+        </table>
+    </body>
+</html>
+EOF
