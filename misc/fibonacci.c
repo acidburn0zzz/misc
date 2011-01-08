b51798b7ed927c5efff51447728283f9bf538e7f
acidrain42 <acidrain1@gmail.com>
2011-01-08 14:41:55 -0500

    Update for fibonacci
    Using ms instead of seconds
    removed millis.c

--- misc/fibonacci.c
+++ misc/fibonacci.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 
-double count = 0;
+long int count = 0;
 
 long int fibonacci(int n) {
     count++;
     return (n <= 1)? n : fibonacci(n-1) + fibonacci(n-2);
 }
 
+int get_interval_in_ms(struct timeval a, struct timeval b) {
+        return (b.tv_sec - a.tv_sec) * 1000 + (b.tv_usec - a.tv_usec) / 1000;
+}
+
 int main(int argc, char *argv[]) {
     long int n;
-    int r, t;
+    int r;
+
+    struct timeval t1, t2;
 
     if (argc == 2) {
         sscanf(argv[1], "%li", &n);
@@ -23,10 +29,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    t = time(NULL);
+    gettimeofday(&t1, NULL);
     r = fibonacci(n);
-    t = time(NULL) - t;
+    gettimeofday(&t2, NULL);
 
-    printf("fibonacci(%li) = %i\n%.0f calculs\nTook %i seconds\n", n, r, count, t);
+    printf("fibonacci(%li) = %i\n%li steps\nTook %i ms\n", n, r, count, get_interval_in_ms(t1, t2));
     return EXIT_SUCCESS;
 }
