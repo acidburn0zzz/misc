ee626a9db957e1cbb60874a15729c3afdb3d3112
acidrain42 <acidrain1@gmail.com>
2011-04-01 00:29:47 -0400

    bfint: small fixes
    Debugging output now build property
    Line comment support is compiled by default
    Corrected 99 bottles to work with line comment

--- brainfuck-interpreter/main.c
+++ brainfuck-interpreter/main.c
@@ -1,90 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
-int fsize(FILE *f);
+/* Comment to remove line comment */
+#define LINE_COMMENT 1
+
+/* Un-comment to add debug output */
+/*#define DEBUG 1*/
+
+/* Should be at least 0x7530 (30 000) */
+#define ARRAY_SIZE 0x8000
+
+#ifdef DEBUG
+void debug_print(const char *fmt, ...) {
+    char msg[1024];
+    va_list va;
+
+    va_start(va, fmt);
+    vsnprintf(msg, sizeof msg, fmt, va);
+    fprintf(stderr, "%s\n", msg);
+}
+#else
+void debug_print(const char *fmt, ...) { }
+#endif
 
 int main(int argc, char *argv[]) {
     unsigned char *ptr, *buf, *_ptr, *_buf, c;
-    int i, dbg = 0;
-    const unsigned int PTR_SIZE = 0x8000;
+    int i, f_size;
     FILE *file;
 
     if (argc < 2) {
-        printf("Usage: %s [-d] file\n", argv[0]);
+        printf("Usage: %s file\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    if (strcmp("-d", argv[1]) == 0)
-        dbg = 1;
-
-    file = fopen(argv[argc-1], "r");
+    file = fopen(argv[argc - 1], "r");
     if (file == NULL) {
         fprintf(stderr, "Error: %s: File not found\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-    _ptr = calloc(1, PTR_SIZE);
-    _buf = malloc(fsize(file));
+    fseek(file, 0, SEEK_END);
+    f_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
-    ptr = _ptr;
-    buf = _buf;
+    ptr = _ptr = calloc(1, ARRAY_SIZE);
+    buf = _buf = malloc(f_size);
 
-    if (fread(buf, fsize(file), sizeof(char), file) != sizeof(char)) {
+    if (fread(buf, f_size, sizeof(char), file) != sizeof(char)) {
         fprintf(stderr, "Error: Unable to read from file: %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-    c = *buf;
-    while (c != 0x00) {
+    while ((c = *buf) != 0x00) {
         switch (c) {
         case '>':
             ++ptr;
-            if (ptr >= _ptr + PTR_SIZE) {
-                puts("\nError: Pointer can't go over 0x8000");
+            if (ptr >= _ptr + ARRAY_SIZE) {
+                fprintf(stderr, "Error: Pointer can't go over 0x%x\n", ARRAY_SIZE);
                 exit(EXIT_FAILURE);
             }
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | Pos. is %ld\n", buf - _buf, c, ptr - _ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | Pos. is %ld\n", buf - _buf, c, ptr - _ptr);
             break;
         case '<':
             --ptr;
             if (ptr < _ptr) {
-                puts("\nError: Pointer can't go under 0");
+                fprintf(stderr, "Error: Pointer can't go under 0\n");
                 exit(EXIT_FAILURE);
             }
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | Pos. is %ld\n", buf - _buf, c, ptr - _ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | Pos. is %ld\n", buf - _buf, c, ptr - _ptr);
             break;
         case '+':
             ++(*ptr);
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | v[%ld]=%d\n", buf - _buf, c, ptr - _ptr, *ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | v[%ld]=%d\n", buf - _buf, c, ptr - _ptr, *ptr);
             break;
         case '-':
             --(*ptr);
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | v[%ld]=%d\n", buf - _buf, c, ptr - _ptr, *ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | v[%ld]=%d\n", buf - _buf, c, ptr - _ptr, *ptr);
             break;
         case '.':
             putchar(*ptr);
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | output '%d' %c\n", buf - _buf, c, *ptr, *ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | output '%d' %c\n", buf - _buf, c, *ptr, *ptr);
             break;
         case ',':
             *ptr = getchar();
-            if (dbg)
-                fprintf(stderr, "(%.3ld): %c | read '%d' %c\n", buf - _buf, c, *ptr, *ptr);
-            c = *(++buf);
+            debug_print("(%.3ld): %c | read '%d' %c\n", buf - _buf, c, *ptr, *ptr);
             break;
         case '[':
             if (*ptr == 0) {
-                i=0;
+                i = 0;
                 c = *(++buf);
                 while (c != ']' || i != 0) {
                     if (c == '[')
@@ -93,13 +98,11 @@ int main(int argc, char *argv[]) {
                         i--;
                     c = *(++buf);
                 }
-            } else {
-                c = *(++buf);
             }
             break;
         case ']':
             if (*ptr != 0) {
-                i=0;
+                i = 0;
                 c = *(--buf);
                 while (c != '[' || i != 0) {
                     if (c == '[')
@@ -108,31 +111,20 @@ int main(int argc, char *argv[]) {
                         i++;
                     c = *(--buf);
                 }
-            } else {
-                c = *(++buf);
             }
             break;
 #ifdef LINE_COMMENT
-        case '#': /*Ajout de moi : # = commentaire de ligne*/
-        case ';': /*Ajout de moi : ; = commentaire de ligne*/
+        case '#':
+        case ';':
             while (c != 0x0a && c != 0x0d && c != 0x00)
                 c = *(++buf);
             break;
 #endif
-        default:
-            c = *(++buf);
         }
+        buf++;
     }
 
     free(_buf);
     free(_ptr);
     return EXIT_SUCCESS;
-}
-
-int fsize(FILE *f) {
-    int size;
-    fseek(f, 0, SEEK_END);
-    size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    return size;
 }
