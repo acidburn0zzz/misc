6b47d985134d2ed8c5fa74e62c0441756e3b9190
acidrain42 <acidrain1@gmail.com>
2011-05-06 15:17:36 -0400

    flac2ogg: replaced shell script by a python script
    Adds support for tags

---
+++ scripts/flac2ogg.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+import sys
+import os
+
+oggenc_opts = '-q 7'
+
+title = None
+artist = None
+album = None
+year = None
+track = None
+genre = None
+
+def usage(prog):
+    print '%s flac_files' % prog
+
+def check_flac():
+    flac = None
+    path = os.getenv('PATH').split(':')
+    for i in path:
+        if os.path.isfile(i + '/flac'):
+            flac = i + '/flac'
+            break
+
+    if flac == None:
+        print 'Error: flac not found'
+        exit(-1)
+
+def check_ogg():
+    oggenc = None
+    path = os.getenv('PATH').split(':')
+    for i in path:
+        if os.path.isfile(i + '/oggenc'):
+            oggenc = i + '/oggenc'
+            break
+
+    if oggenc == None:
+        print 'Error: oggenc not found'
+        exit(-1)
+
+def get_tag(tag, file):
+    tmp = os.popen('metaflac --show-tag=%s %s' % (tag, file)).read()
+    if len(tmp) > 0:
+        return tmp.split('=')[1][:-1]
+    else:
+        return ''
+
+def convert_file(flac_file):
+    #Est-ce un .flac
+    if flac_file[len(flac_file)-5:] != '.flac':
+        print flac_file + ': Not a .flac'
+
+    wav_file = flac_file[:-5] + '.wav'
+    ogg_file = flac_file[:-5] + '.ogg'
+
+    #Creation des options
+    _title = title
+    _artist = artist
+    _album = album
+    _year = year
+    _track = track
+    _genre = genre
+    if _title == None:
+        _title = get_tag('TITLE', flac_file)
+    if _artist == None:
+        _artist = get_tag('ARTIST', flac_file)
+    if _album == None:
+        _album = get_tag('ALBUM', flac_file)
+    if _year == None:
+        _year = get_tag('DATE', flac_file)
+    if _track == None:
+        _track = get_tag('TRACKNUMBER', flac_file) + '/' + get_tag('TRACKTOTAL', flac_file)
+    if _genre == None:
+        _genre = get_tag('GENRE', flac_file)
+
+    tag_opts = '--title "%s" --artist "%s" --album "%s" --date "%s" --tracknum "%s" --genre "%s"' % (_title, _artist, _album, _year, _track, _genre)
+
+    #Flac 2 wav
+    os.system('flac -df -o %s %s' % (wav_file, flac_file))
+
+    #Wav 2 ogg
+    cmd = 'oggenc %s %s %s -o %s' % (oggenc_opts, tag_opts, wav_file, ogg_file)
+    os.system(cmd)
+
+    os.system('rm %s' % wav_file)
+
+if __name__ == '__main__':
+    #Verification de l'existence de oggenc et flac
+    check_flac()
+    check_ogg()
+
+    for i in sys.argv[1:]:
+        convert_file(i)
--- scripts/flac2ogg.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-for i in $( ls *.flac ); do
-    flac -cd $i | oggenc -q 7 -o `echo $i | awk -F . '{print $1 ".ogg"}'` -
-done
