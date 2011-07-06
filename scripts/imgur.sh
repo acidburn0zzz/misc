803edfc3cbf064a7f0fb357234ac3a400540ef4b
acidrain42 <acidrain1@gmail.com>
2011-07-06 10:03:58 -0400

    Minor fixes to scripts

--- scripts/imgur.sh
+++ scripts/imgur.sh
@@ -2,6 +2,10 @@
 
 COUNT=1;
 
+if [ $# -ge 2 ]; then
+    COUNT=$2
+fi
+
 for i in $(wget $1 -O - | tidy 2>/dev/null | grep -o 'http://.*s\.jpg' | sed 's/s\.jpg/.jpg/'); do
     wget $i -O $(printf %03d $COUNT).jpg;
     let COUNT=COUNT+1;
--- scripts/lsflash.sh
+++ scripts/lsflash.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-lsof -n -P | grep 'Flash[[:alnum:]]\{6\}' | awk '{ print "/proc/" $2 "/fd/" substr($4, 1, length($4)-1) }'
+lsof -n -P | grep 'Flash[[:alnum:]]\{6\}' | awk '{ print "/proc/" $2 "/fd/" substr($4, 1, length($4)-1) }' | grep '[0-9]$'
