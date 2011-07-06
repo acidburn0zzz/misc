c6c882b6b9feb81ae1d331e56ef72be82bbe3957
acidrain42 <acidrain1@gmail.com>
2011-07-06 10:03:25 -0400

    Modified flac2mp3.py to make it work under windows

--- scripts/flac2mp3.py
+++ scripts/flac2mp3.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 #-*- coding: utf-8 -*-
 import sys
 import os
@@ -12,31 +12,29 @@ year = None
 track = None
 genre = None
 
+win32 = False
+
 def usage(prog):
     print '%s flac_files' % prog
 
-def check_flac():
-    flac = None
-    path = os.getenv('PATH').split(':')
-    for i in path:
-        if os.path.isfile(i + '/flac'):
-            flac = i + '/flac'
-            break
+def check_exe(name):
+    exe = None
+    ext = ''
 
-    if flac == None:
-        print 'Error: flac not found'
-        exit(-1)
+    if win32:
+        ext = '.exe'
 
-def check_lame():
-    lame = None
-    path = os.getenv('PATH').split(':')
-    for i in path:
-        if os.path.isfile(i + '/lame'):
-            lame = i + '/lame'
-            break
+    if os.path.isfile(name + ext):
+        exe = name + ext
+    else:
+        path = os.getenv('PATH').split(';' if win32 else ':')
+        for i in path:
+            if os.path.isfile(i + '/' + name + ext):
+                exe = i + '/' + name + ext
+                break
 
-    if lame == None:
-        print 'Error: lame not found'
+    if exe == None:
+        print 'Error: %s not found' % name
         exit(-1)
 
 def get_tag(tag, file):
@@ -87,9 +85,12 @@ def convert_file(flac_file):
     os.system('rm %s' % wav_file)
 
 if __name__ == '__main__':
+    if sys.platform == 'win32':
+        win32 = True
+
     #Verification de l'existence de lame et flac
-    check_flac()
-    check_lame()
+    check_exe('flac')
+    check_exe('lame')
 
     for i in sys.argv[1:]:
         convert_file(i)
