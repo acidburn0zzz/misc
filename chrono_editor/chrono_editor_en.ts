cc11bbb22a80d892f9e8b1bdc88b486a58ead85c
acidrain42 <acidrain1@gmail.com>
2014-01-27 09:49:41 -0500

    Updated chrono editor translation file

--- chrono_editor/chrono_editor_en.ts
+++ chrono_editor/chrono_editor_en.ts
@@ -4,89 +4,89 @@
 <context>
     <name>Vue</name>
     <message>
-        <location filename="vue.cpp" line="229"/>
+        <location filename="vue.cpp" line="243"/>
         <source>Ouvrir un fichier</source>
         <translation>Open a file</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="229"/>
+        <location filename="vue.cpp" line="243"/>
         <source>Fichier SRAM (*.srm);;Tous (*.*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="233"/>
+        <location filename="vue.cpp" line="247"/>
         <source>&amp;Ouvrir</source>
         <translation>&amp;Open</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="234"/>
+        <location filename="vue.cpp" line="248"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="235"/>
+        <location filename="vue.cpp" line="249"/>
         <source>Ouvrir une Save State</source>
         <translation>Open a Save State</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="238"/>
+        <location filename="vue.cpp" line="252"/>
         <source>&amp;Enregistrer</source>
         <translation>&amp;Save</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="239"/>
+        <location filename="vue.cpp" line="253"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="240"/>
+        <location filename="vue.cpp" line="254"/>
         <source>Enregistrer la Save State</source>
         <translation>Save the Save State</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="243"/>
+        <location filename="vue.cpp" line="257"/>
         <source>&amp;Quitter</source>
         <translation>&amp;Quit</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="244"/>
+        <location filename="vue.cpp" line="258"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="245"/>
+        <location filename="vue.cpp" line="259"/>
         <source>Quitter</source>
         <translation>Quit</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="248"/>
+        <location filename="vue.cpp" line="262"/>
         <source>&amp;Fichier</source>
         <translation>&amp;File</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="254"/>
+        <location filename="vue.cpp" line="268"/>
         <source>Ã &amp;Propos de Chrono Editor</source>
         <oldsource>À Propos de Chrono Editor</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="255"/>
+        <location filename="vue.cpp" line="269"/>
         <source>Ctrl+A</source>
         <translation>Ctrl+A</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="257"/>
+        <location filename="vue.cpp" line="271"/>
         <source>Ã Propos de &amp;Qt</source>
         <oldsource>À Propos de &amp;Qt</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="258"/>
+        <location filename="vue.cpp" line="272"/>
         <source>Ctrl+T</source>
         <translation>Ctrl+T</translation>
     </message>
     <message>
-        <location filename="vue.cpp" line="261"/>
+        <location filename="vue.cpp" line="275"/>
         <source>&amp;Aide</source>
         <translation>&amp;Help</translation>
     </message>
