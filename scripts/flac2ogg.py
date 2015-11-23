ed841ff810198cccd424602723fbdbb90dc1f698
Mathieu Lemay <mlemay@geemedia.com>
2015-11-23 17:32:41 -0500

    flac2ogg: Speedup by using threads and code cleanup

--- scripts/flac2ogg.py
+++ scripts/flac2ogg.py
@@ -1,56 +1,57 @@
 #! /usr/bin/env python
-#-*- coding: utf-8 -*-
-import sys
+
 import os
+import sys
+from concurrent.futures import ThreadPoolExecutor
 
 oggenc_opts = '-q 10'
 
-win32 = False
 
 def usage(prog):
-    print('%s flac_files' % (prog))
+    print('{} flac_files'.format(prog))
+
 
 def check_exe(name):
     exe = None
     ext = ''
 
-    if win32:
+    if sys.platform == 'win32':
         ext = '.exe'
 
     if os.path.isfile(name + ext):
         exe = name + ext
     else:
-        path = os.getenv('PATH').split(';' if win32 else ':')
+        path = os.getenv('PATH').split(';' if sys.platform == 'win32' else ':')
         for i in path:
             if os.path.isfile(i + '/' + name + ext):
                 exe = i + '/' + name + ext
                 break
 
-    if exe == None:
-        print('Error: %s not found' % (name))
+    if exe is None:
+        print('Error: {} not found'.format(name))
         exit(-1)
 
+
 def get_tag(tag, file):
-    tmp = os.popen('metaflac --show-tag=%s "%s"' % (tag, file)).read()
+    tmp = os.popen('metaflac --show-tag={} "{}"'.format(tag, file)).read()
     if len(tmp) > 0:
         return tmp.split('=')[1][:-1]
     else:
         return ''
 
+
 def convert_file(flac_file):
     if not os.path.isfile(flac_file):
-        print('%s: file not found' % (flac_file))
+        print('{}: file not found'.format(flac_file))
         return
 
-    #Est-ce un .flac
-    if flac_file[len(flac_file)-5:] != '.flac':
-        print('%s: not a flac' % (flac_file))
+    if os.path.splitext(flac_file)[1] != '.flac':
+        print('{}: not a flac'.format(flac_file))
         return
 
-    wav_file = flac_file[:-5] + '.wav'
-    ogg_file = flac_file[:-5] + '.ogg'
+    wav_file = os.path.splitext(flac_file)[0] + '.wav'
+    ogg_file = os.path.splitext(flac_file)[0] + '.ogg'
 
-    #Creation des options
     title = get_tag('TITLE', flac_file)
     artist = get_tag('ARTIST', flac_file)
     album = get_tag('ALBUM', flac_file)
@@ -58,25 +59,23 @@ def convert_file(flac_file):
     track = get_tag('TRACKNUMBER', flac_file) + '/' + get_tag('TRACKTOTAL', flac_file)
     genre = get_tag('GENRE', flac_file)
 
-    tag_opts = '--title "%s" --artist "%s" --album "%s" --date "%s" --tracknum "%s" --genre "%s"' % (title, artist, album, year, track, genre)
+    tag_opts = '--title "{}" --artist "{}" --album "{}" --date "{}" --tracknum "{}" --genre "{}"'.format(title, artist, album, year, track, genre)
 
-    #Flac 2 wav
-    os.system('flac -df -o "%s" "%s"' % (wav_file, flac_file))
+    os.system('flac -df -o "{}" "{}"'.format(wav_file, flac_file))
 
-    #Wav 2 ogg
-    cmd = 'oggenc %s %s "%s" -o "%s"' % (oggenc_opts, tag_opts, wav_file, ogg_file)
+    cmd = 'oggenc {} {} "{}" -o "{}"'.format(oggenc_opts, tag_opts, wav_file, ogg_file)
     os.system(cmd)
 
-    #rm wav
-    os.system('rm "%s"' % wav_file)
+    os.remove(wav_file)
 
-if __name__ == '__main__':
-    if sys.platform == 'win32':
-        win32 = True
 
-    #Verification de l'existence de lame et flac
+def main():
     check_exe('flac')
     check_exe('oggenc')
 
-    for i in sys.argv[1:]:
-        convert_file(i)
+    with ThreadPoolExecutor(max_workers=8) as e:
+        for i in sys.argv[1:]:
+            e.submit(convert_file, i)
+
+if __name__ == '__main__':
+    main()
